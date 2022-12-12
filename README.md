1527a155adacd792b6079b325665aeadd17eb26d
taserbeat <taserbeat@gmail.com>
2022-12-12 22:32:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# 概要
+
+[[2022 年決定版] Vue3 x Typescript によるアプリケーションづくり](https://www.udemy.com/course/vue3-typescript/)のサンプルコード
+
+# 開発環境
+
+| 名称    | 条件     |
+| ------- | -------- |
+| Node.js | v16.14.0 |
+| Yarn    | 1.22.19  |
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- ロジックを記述していく -->
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<!-- HTMLの画面要素を記述していく -->
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<!-- templateに対して適用するスタイルを記述する -->
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
